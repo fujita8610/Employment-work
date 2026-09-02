e81baf7c6020293313fc527deb8ac7608233b678
fujita8610 <haruto.2004.5.6@gmail.com>
2026-09-02 13:58:58 +0900

    コメントと、プレイヤーの改良

--- カードアクションキング/Souce/Scene/Battle/Turn/TurnManager.cpp
+++ カードアクションキング/Souce/Scene/Battle/Turn/TurnManager.cpp
@@ -3,6 +3,7 @@
 // BattleManager
 #include "../BattleManager.h"
 
+// 初期化
 bool TurnManager::Init(BattleManager* battleManager)
 {
     if (battleManager == nullptr)
@@ -12,10 +13,20 @@ bool TurnManager::Init(BattleManager* battleManager)
 
     m_battleManager = battleManager;
 
+	//ターンの初期化
     m_playerTurn.Init(battleManager);
     m_enemyTurn.Init(battleManager);
 
+	//初期状態
     m_currentTurn = TurnType::None;
+
+    // 現在はテストとして
+   // Player1 = Human
+   // Player2 = AI
+    m_player1Controller = ControllerType::Human;
+    m_player2Controller = ControllerType::AI;
+
+    //ターン情報
     m_turnCount = 0;
     m_started = false;
 
@@ -35,7 +46,8 @@ void TurnManager::StartBattle()
     m_turnCount = 1;
 
     // 最初はプレイヤーターン
-    m_currentTurn = TurnType::Player;
+    //今後は先攻後攻を実装
+    m_currentTurn = TurnType::Player1;
 
     m_playerTurn.Start();
 }
@@ -50,7 +62,7 @@ void TurnManager::Update()
 
     switch (m_currentTurn)
     {
-    case TurnType::Player:
+    case TurnType::Player1:
 
         m_playerTurn.Update();
 
@@ -62,7 +74,7 @@ void TurnManager::Update()
         break;
 
 
-    case TurnType::Enemy:
+    case TurnType::Player2:
 
         m_enemyTurn.Update();
 
@@ -85,10 +97,10 @@ void TurnManager::ChangeTurn()
 {
     switch (m_currentTurn)
     {
-    case TurnType::Player:
+    case TurnType::Player1:
 
         // 敵ターンへ
-        m_currentTurn = TurnType::Enemy;
+        m_currentTurn = TurnType::Player2;
 
         m_enemyTurn.Reset();
         m_enemyTurn.Start();
@@ -96,13 +108,13 @@ void TurnManager::ChangeTurn()
         break;
 
 
-    case TurnType::Enemy:
+    case TurnType::Player2:
 
         // 1ターン終了
         m_turnCount++;
 
         // プレイヤーターンへ
-        m_currentTurn = TurnType::Player;
+        m_currentTurn = TurnType::Player1;
 
         m_playerTurn.Reset();
         m_playerTurn.Start();
@@ -125,14 +137,14 @@ void TurnManager::EndCurrentTurn()
 
     switch (m_currentTurn)
     {
-    case TurnType::Player:
+    case TurnType::Player1:
 
         m_playerTurn.End();
 
         break;
 
 
-    case TurnType::Enemy:
+    case TurnType::Player2:
 
         m_enemyTurn.End();
 
@@ -153,20 +165,83 @@ TurnType TurnManager::GetCurrentTurn() const
 // プレイヤーターンか
 bool TurnManager::IsPlayerTurn() const
 {
-    return m_currentTurn == TurnType::Player;
+    return m_currentTurn == TurnType::Player1;
 }
 
 // エネミーターンか
 bool TurnManager::IsEnemyTurn() const
 {
-    return m_currentTurn == TurnType::Enemy;
+    return m_currentTurn == TurnType::Player2;
 }
+
+// プレイヤー操作方式が人間か
+bool TurnManager::IsHumanTurn() const
+{
+    return GetControllerType(m_currentTurn)
+        == ControllerType::Human;
+}
+
+// プレイヤー操作方式がAIか
+bool TurnManager::IsAITurn() const
+{
+    return GetControllerType(m_currentTurn)
+        == ControllerType::AI;
+}
+
+// プレイヤー操作方式が自動か
+bool TurnManager::IsAutoTurn() const
+{
+    return GetControllerType(m_currentTurn)
+        == ControllerType::Auto;
+}
+
 
 // ターン番号
 int TurnManager::GetTurnCount() const
 {
     return m_turnCount;
 }
+
+//操作方式
+ControllerType TurnManager::GetControllerType(
+    TurnType turn) const
+{
+    switch (turn)
+    {
+    case TurnType::Player1:
+
+        return m_player1Controller;
+
+
+    case TurnType::Player2:
+
+        return m_player2Controller;
+
+
+    case TurnType::None:
+
+    default:
+
+        return ControllerType::None;
+    }
+}
+
+//プレイヤー１の操作方式設定
+void TurnManager::SetPlayer1Controller(
+    ControllerType controller)
+{
+    m_player1Controller = controller;
+}
+
+//プレイヤー２の操作方式設定
+void TurnManager::SetPlayer2Controller(
+    ControllerType controller)
+{
+    m_player2Controller = controller;
+}
+
+
+
 
 // 終了処理
 void TurnManager::Release()
@@ -181,4 +256,7 @@ void TurnManager::Release()
     m_turnCount = 0;
 
     m_started = false;
+
+    m_player1Controller = ControllerType::None;
+    m_player2Controller = ControllerType::None;
 }
--- カードアクションキング/Souce/Scene/Battle/Turn/TurnManager.h
+++ カードアクションキング/Souce/Scene/Battle/Turn/TurnManager.h
@@ -13,32 +13,61 @@ public:
 
     TurnManager() = default;
 
-    //初期化
+    // 初期化
     bool Init(BattleManager* battleManager);
 
-    //更新
+    // 更新
     void Update();
 
-    //終了処理
-    void Release();
+    // 戦闘開始
+    void StartBattle();
 
-    //現在のターン取得
-    TurnType GetCurrentTurn() const;
-
-    //プレイヤーターン
-    bool IsPlayerTurn() const;
-
-    //エネミーターン
-    bool IsEnemyTurn() const;
-
-    //ターン終了
+    // 現在のターンを終了
     void EndCurrentTurn();
 
-    //ターン数
+    // 終了処理
+    void Release();
+
+
+    //========================================
+    // ターン情報
+    //========================================
+
+    // 現在のターン
+    TurnType GetCurrentTurn() const;
+
+    // 現在のターン番号
     int GetTurnCount() const;
 
-    //先頭開始
-    void StartBattle();
+
+    //========================================
+    // ターン判定
+    //========================================
+
+    bool IsPlayerTurn() const;
+    bool IsEnemyTurn() const;
+
+    //========================================
+    // プレイヤータイプ判定
+    //========================================
+    bool IsHumanTurn() const;
+    bool IsAITurn() const;
+    bool IsAutoTurn() const;
+
+    //========================================
+    // 操作方式
+    //========================================
+
+    // 指定プレイヤーの操作方式取得
+    ControllerType GetControllerType(TurnType turn) const;
+
+
+    //========================================
+    // 操作方式設定
+    //========================================
+
+    void SetPlayer1Controller(ControllerType controller);
+    void SetPlayer2Controller(ControllerType controller);
 
 private:
     //ターンの切り替え
@@ -47,18 +76,24 @@ private:
 
 private:
 
+	// バトルマネージャー
     BattleManager* m_battleManager = nullptr;
 
+    //ターンの取得
     PlayerTurn m_playerTurn;
     EnemyTurn m_enemyTurn;
 
+	// 現在のターン
     TurnType m_currentTurn = TurnType::None;
 
+	// プレイヤーの操作方式
     ControllerType m_player1Controller = ControllerType::Human;
     ControllerType m_player2Controller = ControllerType::AI;
 
+	// ターン番号
     int m_turnCount = 0;
 
+	// 戦闘開始フラグ
     bool m_started = false;
 
 };
